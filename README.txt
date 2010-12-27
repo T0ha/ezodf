883fe7ff5334a3bd5738d4d3f5e7ee9ec3725be0
mozman <mozman@gmx.at>
2010-12-27 12:25:01 +0100

    init commit

---
+++ README.txt
@@ -0,0 +1,59 @@
+
+Abstract
+========
+
+'ezodf' is a Python package to create new or open existing OpenDocumentFormat files
+and extract, add, modify or delete document data.
+
+a simple example::
+
+    import ezodf
+
+    ods = ezodf.ods('testspreadsheet.ods')
+	sheet = ezodf.Spreadsheet(ods, 'SHEET')
+	sheet[0, 0] = ezodf.Cell("Textcell")
+    ods.save()
+
+for more examples see: /examples folder
+
+Dependencies
+============
+
+There are no other dependencies than the Standard Python Library.
+
+The main target platform is Python 3.1+, perhaps I will integrate Python 2.6+ support,
+if there is no great effort to achieve this goal.
+
+
+Installation
+============
+
+with pip::
+
+    pip install ezodf
+
+or from source::
+
+    python setup.py install
+
+Tests
+=====
+
+run tests ::
+
+    python runtests.py
+
+on Windows::
+
+    runtests.bat
+
+Documentation
+=============
+
+http://packages.python.org/ezodf
+
+send feedback to mozman@gmx.at
+
+ezodf can be found on bitbucket.org at:
+
+http://bitbucket.org/mozman/ezodf
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author:  mozman
+# Purpose: setup
+# Created: 27.12.2010
+#
+#    Copyright (C) 2010  Manfred Moitzi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from distutils.core import setup
+
+VERSION = '0.1.0'
+AUTHOR_NAME = 'Manfred Moitzi'
+AUTHOR_EMAIL = 'mozman@gmx.at'
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return "File '%s' not found.\n" % fname
+
+setup(name='ezodf',
+    version=VERSION,
+    description='A Python package to create/manipulate OpenDocumentFormat files.',
+    author=AUTHOR_NAME,
+    url='http://bitbucket.org/mozman/ezodf',
+    download_url='http://bitbucket.org/mozman/ezodf/downloads',
+    author_email=AUTHOR_EMAIL,
+    packages=['ezodf'],
+    provides=['ezodf'],
+    long_description=read('README.TXT')+read('NEWS.TXT'),
+    platforms="OS Independent",
+    license="GPLv3",
+    classifiers=[
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.1",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+     )
