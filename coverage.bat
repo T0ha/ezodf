915b67cfdf8cd75ebc556ec925d1ba39a465f7ea
mozman <mozman@gmx.at>
2011-02-06 19:36:16 +0100

    minor edits

--- coverage.bat
+++ coverage.bat
@@ -1,7 +1,7 @@
 @set PYTHONPATH=%CD%
-@set NOSE_WITH_COVERAGE=YES
+@set NOSE_WITH_COVERAGE=true
 @set NOSE_COVER_PACKAGE=ezodf
 
-@C:\Python31\python.exe C:\Python31\Scripts\nosetests-script.py %*
+@C:\Python31\python.exe C:\Python31\Scripts\nosetests-script.py %* 
 
 @set NOSE_WITH_COVERAGE=
