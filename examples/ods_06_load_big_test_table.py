f4b0e78a729c115561b2d8dcc907976e7c35d6b0
mozman <mozman@gmx.at>
2012-06-07 07:45:44 +0200

    added example for big tables

---
+++ examples/ods_06_load_big_test_table.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Purpose: swap row/columns of a table
+# Created: 28.05.2012
+# Copyright (C) 2012, Manfred Moitzi
+# License: GPLv3
+from __future__ import unicode_literals, print_function, division
+__author__ = "mozman <mozman@gmx.at>"
+
+import ezodf
+
+# open spreadsheet document
+doc = ezodf.opendoc("big-test-table.ods")
+    
+print("Spreadsheet contains %d sheets.\n" % len(doc.sheets))
+for sheet in doc.sheets:
+    print("Sheet name: '%s'" % sheet.name)
+    print("Size of Sheet : (%d, %d)" % (sheet.nrows(), sheet.ncols()) )
+    print("-"*40)
+    
+doc.save()
