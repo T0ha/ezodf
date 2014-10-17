cb61b801e975b5161261a99e35f5f2f6a68006f7
Shvein Anton <t0hashvein@gmail.com>
2014-10-17 11:24:43 +0600

    Deleted IDE and windows specific files

--- coverage.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-set PYTHONPATH=%CD%
-set NOSE_WITH_COVERAGE=true
-set NOSE_COVER_PACKAGE=ezodf
-set NOSE=C:\Python33\Scripts\nosetests-script.py
-
-py -3.3 %NOSE% %* 
-
-set NOSE_WITH_COVERAGE=
--- test.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-ECHO Running Python 2.7 tests
-CALL test27.bat
-ECHO Running Python 3.3 tests
-CALL test33.bat
