588e6bdf4241c5c5b06bb915e28db4bfa5dc13a7
Shvein Anton <t0hashvein@gmail.com>
2014-11-05 16:38:45 +0500

    Fixed attribute error

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -14,95 +14,153 @@ except ImportError:
     import unittest
 
 # trusted or separately tested modules
-from ezodf.xmlns import CN, etree
+from ezodf.xmlns import CN, etree, wrap
 from lxml.etree import Element
 from ezodf.base import GenericWrapper
 
 # objects to test
 from ezodf.variables import SimpleVariables, SimpleVariable
+from ezodf.variables import SimpleVariableInstance
 
-SIMPLE_VARIABLE_DECL='<text:variable-decl '\
+## Contacts decloration {{{1
+SIMPLE_VARIABLE_DECL = '<text:variable-decl '\
+    'xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" '\
+    'xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" '\
     'office:value-type="string" '\
     'text:name="simple1"/>'
 
-SIMPLE_VARIABLE_DECLS='<text:variable-decls>'\
+SIMPLE_VARIABLE_DECLS = '<text:variable-decls '\
+    'xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" '\
+    'xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" '\
+    '>'\
     '<text:variable-decl office:value-type="string" text:name="simple1"/>'\
     '</text:variable-decls>'
+SIMPLE_VARIABLE_SET = '<text:variable-set '\
+    'xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" '\
+    'xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" '\
+    'text:name="simple1" '\
+    'office:value-type="string">'\
+    'simple1</text:variable-set>'
 
-USERFIELD_VARIABLE_DECL='<text:user-field-decl'\
+USERFIELD_VARIABLE_DECL = '<text:user-field-decl'\
     ' office:value-type="string"'\
     ' office:string-value="user_field1_copy"'\
     ' text:name="user_field1"/'
 
-USERFIELD_VARIABLE_DECLS='<text:user-field-decls>'\
+USERFIELD_VARIABLE_DECLS = '<text:user-field-decls>'\
     '<text:user-field-decl office:value-type="string" office:string-value="user_field1_copy" text:name="user_field1"/>'\
     '</text:user-field-decls>'
 
-FULL_BODY="""
-<office:body>
- <office:text>
-  <text:variable-decls>
-   <text:variable-decl office:value-type="string" text:name="simple1"/>
-  </text:variable-decls>
-  <text:sequence-decls>
-   <text:sequence-decl text:display-outline-level="0" text:name="Illustration"/>
-   <text:sequence-decl text:display-outline-level="0" text:name="Table"/>
-   <text:sequence-decl text:display-outline-level="0" text:name="Text"/>
-   <text:sequence-decl text:display-outline-level="0" text:name="Drawing"/>
-  </text:sequence-decls>
-  <text:user-field-decls>
-   <text:user-field-decl office:value-type="string" office:string-value="user_field1_copy" text:name="user_field1"/>
-  </text:user-field-decls>
-  <text:p text:style-name="Standard"><text:variable-set text:name="simple1" office:value-type="string">simple1</text:variable-set></text:p>
-  <text:p text:style-name="Standard"><text:variable-get text:name="simple1">simple1</text:variable-get></text:p>
-  <text:p text:style-name="Standard"/>
-  <text:p text:style-name="Standard"><text:user-field-get text:name="user_field1">user_field1_copy</text:user-field-get></text:p>
-  <text:p text:style-name="Standard"><text:user-field-get text:name="user_field1">user_field1_copy</text:user-field-get></text:p>
- </office:text>
-</office:body>
-"""
+# }}}
 
-class TestSimpleVariables(unittest.TestCase):
-    def test_bare(self):
+class TestSimpleVariables(unittest.TestCase):  # {{{1
+    def test_bare(self):  # {{{2
         """docstring for setUp"""
         variables = SimpleVariables()
         self.assertTrue(isinstance(variables, GenericWrapper))
         self.assertEqual(variables.xmlnode.tag, CN('text:variable-decls'))
 
-    def test_init_xmlroot(self):
+    def test_init_xmlroot(self):  # {{{2
         node = etree.Element(CN('text:variable-decls'), test="variables")
         variables = SimpleVariables(xmlnode=node)
         self.assertTrue(isinstance(variables, GenericWrapper))
-        self.assertEqual(variables.xmlnode.tag, CN('text:variables'))
+        self.assertEqual(variables.xmlnode.tag, CN('text:variable-decls'))
         self.assertEqual(variables.xmlnode.get('test'), "variables")
 
-    def test_simple_list(self):
-        """docstring for setUp"""
-        variables = SimpleVariables()
+    def test_init_XML(self):  # {{{2
+        node = etree.XML(SIMPLE_VARIABLE_DECLS)
+        variables = SimpleVariables(xmlnode=node)
         self.assertTrue(isinstance(variables, GenericWrapper))
         self.assertEqual(variables.xmlnode.tag, CN('text:variable-decls'))
 
-class TestSimpleVariable(unittest.TestCase):
-    def test_bare(self):
+    def test_simple_variable_dict(self):  # {{{2
+        """docstring for setUp"""
+        node = etree.XML(SIMPLE_VARIABLE_DECLS)
+        variables = SimpleVariables(xmlnode=node)
+        vnode = etree.XML(SIMPLE_VARIABLE_DECL)
+        variable = SimpleVariable(xmlnode=vnode)
+        self.assertTrue(isinstance(variables, GenericWrapper))
+        self.assertEqual(variables.xmlnode.tag, CN('text:variable-decls'))
+        self.assertEqual(variables[u'simple1'].type, variable.type)
+        self.assertEqual(variables[u'simple1'].name, variable.name)
+
+
+
+
+class TestSimpleVariable(unittest.TestCase):  # {{{1
+    def test_bare(self):  # {{{2
         """docstring for setUp"""
         variable = SimpleVariable()
         self.assertTrue(isinstance(variable, GenericWrapper))
         self.assertEqual(variable.xmlnode.tag, CN('text:variable-decl'))
 
-    def test_init_xmlroot(self):
+    def test_init_xmlroot(self):  # {{{2
         node = etree.Element(CN('text:variable-decl'), test="variable")
         variables = SimpleVariables(xmlnode=node)
         self.assertTrue(isinstance(variables, GenericWrapper))
         self.assertEqual(variables.xmlnode.tag, CN('text:variable-decl'))
         self.assertEqual(variables.xmlnode.get('test'), "variable")
 
-    def test_init_XML(self):
+    def test_init_XML(self):  # {{{2
         node = etree.XML(SIMPLE_VARIABLE_DECL)
-        span = SimpleVariable(xmlnode=node)
-        self.assertTrue(isinstance(span, GenericWrapper))
-        self.assertEqual(span.xmlnode.tag, CN('text:variable-decl'))
+        variable = SimpleVariable(xmlnode=node)
+        self.assertTrue(isinstance(variable, GenericWrapper))
+        self.assertEqual(variable.xmlnode.tag, CN('text:variable-decl'))
+        self.assertEqual(variable.type, u'string')
+        self.assertEqual(variable.name, u'simple1')
+
+    def test_get_string(self):
+        """
+        Gets variable
+        This is not exact unittest, but it's very usefull
+        """
+        decls_node = etree.XML(SIMPLE_VARIABLE_DECLS)
+        decls = SimpleVariables(xmlnode=decls_node)
+        node = etree.XML(SIMPLE_VARIABLE_SET)
+        variable = SimpleVariableInstance(xmlnode=node)
+        self.assertEqual(decls[u'simple1'].get(), u"simple1")
+
+
+class TestSimpleVariableInstance(unittest.TestCase):  # {{{1
+    def test_bare(self):  # {{{2
+        """docstring for setUp"""
+        variable = SimpleVariableInstance()
+        self.assertTrue(isinstance(variable, GenericWrapper))
+        self.assertEqual(variable.xmlnode.tag, CN('text:variable-set'))
+
+    def test_init_xmlroot(self):  # {{{2
+        node = etree.Element(CN('text:variable-set'), test="variable")
+        variables = SimpleVariableInstance(xmlnode=node)
+        self.assertTrue(isinstance(variables, GenericWrapper))
+        self.assertEqual(variables.xmlnode.tag, CN('text:variable-set'))
+        self.assertEqual(variables.xmlnode.get('test'), "variable")
+
+    def test_init_XML(self):  # {{{2
+        node = etree.XML(SIMPLE_VARIABLE_SET)
+        variable = SimpleVariableInstance(xmlnode=node)
+        self.assertTrue(isinstance(variable, GenericWrapper))
+        self.assertEqual(variable.xmlnode.tag, CN('text:variable-set'))
+        self.assertEqual(variable.type, u'string')
+        self.assertEqual(variable.name, u'simple1')
+
+    def test_init_with_decls(self):  # {{{2
+        decls_node = etree.XML(SIMPLE_VARIABLE_DECLS)
+        decls = SimpleVariables(xmlnode=decls_node)
+        node = etree.XML(SIMPLE_VARIABLE_SET)
+        variable = SimpleVariableInstance(xmlnode=node)
+        self.assertTrue(isinstance(variable, GenericWrapper))
+        self.assertEqual(variable.xmlnode.tag, CN('text:variable-set'))
+        self.assertIn(variable, decls[u'simple1'].instances)
+
+    def test_get_string(self):
+        """
+        Gets variable
+        """
+        node = etree.XML(SIMPLE_VARIABLE_SET)
+        variable = SimpleVariableInstance(xmlnode=node)
+        self.assertEqual(variable.get(), u"simple1")
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # {{{1
     unittest.main()
