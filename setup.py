bab11e6b490681b039fa108b5166d5e51f8df408
Shvein Anton <t0hashvein@gmail.com>
2014-11-24 10:25:11 +0500

    Fixed credentials

--- setup.py
+++ setup.py
@@ -16,6 +16,9 @@ from version import VERSION
 AUTHOR_NAME = 'Manfred Moitzi'
 AUTHOR_EMAIL = 'mozman@gmx.at'
 
+MAINTAINER_NAME = 'Anton Shvein'
+MAINTAINER_EMAIL = 't0hashvein@gmail.com'
+
 def read(fname):
     try:
         return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -23,28 +26,33 @@ def read(fname):
         return "File '%s' not found.\n" % fname
 
 setup(name='ezodf',
-    version=VERSION,
-    description='A Python package to create/manipulate OpenDocumentFormat files.',
-    author=AUTHOR_NAME,
-    url='http://bitbucket.org/mozman/ezodf',
-    download_url='http://bitbucket.org/mozman/ezodf/downloads',
-    author_email=AUTHOR_EMAIL,
-    packages=['ezodf'],
-    provides=['ezodf'],
-    install_requires=['lxml'],
-    keywords=['ODF', 'OpenDocumentFormat', 'OpenOffice', 'LibreOffice'],
-    long_description=read('README.rst')+read('NEWS.rst'),
-    platforms="OS Independent",
-    license="MIT License",
-    classifiers=[
-    "Development Status :: 3 - Alpha",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.3",
-    "Intended Audience :: Developers",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Topic :: Office/Business :: Office Suites",
-    ]
-     )
+      version=VERSION,
+      description='A Python package to create/manipulate OpenDocumentFormat files.',
+      author=AUTHOR_NAME,
+      url='https://github.com/T0ha/ezodf',
+      download_url='https://github.com/T0ha/ezodf/releases',
+      author_email=AUTHOR_EMAIL,
+      maintainer=MAINTAINER_NAME,
+      maintainer_email=MAINTAINER_EMAIL,
+      packages=['ezodf'],
+      provides=['ezodf'],
+      install_requires=['weakrefset', 'lxml'],
+      keywords=['ODF', 'OpenDocumentFormat', 'OpenOffice', 'LibreOffice'],
+      long_description=read('README.rst')+read('NEWS.rst'),
+      platforms="OS Independent",
+      license="MIT License",
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Intended Audience :: Developers",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Office/Business :: Office Suites",
+      ]
+      )
